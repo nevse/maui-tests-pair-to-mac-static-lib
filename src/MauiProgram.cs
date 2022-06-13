fe92d3de8200958b3dac9472571232fcba940359
Eugene Novikov <nov.eugen@gmail.com>
2022-06-13 11:45:16 +0400

    init

---
+++ src/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using DevExpress.Maui.Editors;
+using DevExpress.Maui.CollectionView;
+using DevExpress.Maui;
+
+namespace EditorsGetStarted
+{
+	public static class MauiProgram
+	{
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.UseDevExpress()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+
+			return builder.Build();
+		}
+	}
+}
